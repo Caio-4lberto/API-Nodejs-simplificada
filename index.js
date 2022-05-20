7f9a795dcd42b43aacd7dd340beb92cd99aaddb4
Caio-4lberto <caioalbertodasilva@gmail.com>
2022-05-20 15:56:59 -0300

    Consumindo API de forma simples

---
+++ index.js
@@ -0,0 +1,24 @@
+/*_____________________________________________
+               API - Exemplos
+_______________________________________________
+       https://api.github.com/users/
+       https://pokeapi.co/api/v2/pokemon
+       https://rickandmortyapi.com/api/character
+       https://funtranslations.com/api/
+       https://viacep.com.br/ws/01001000/json/
+_______________________________________________*/
+
+
+
+const axios = require('axios');
+
+async function usersGitHub() {
+    const response = await axios.get('https://api.github.com/users/caio-4lberto');
+    const data_info = response.data
+    const info = response.data.id  
+    console.log(info)
+    return data_info
+}
+
+
+usersGitHub();
